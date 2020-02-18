9c0103e2260f89e09eef6a3487a6e30ea1659981
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-02-18 09:05:02 +0530

    Game determines the winner as firstPlayer if they beat the secondPlayer. [WIP]

--- src/main/java/com/ashraymehta/rockpaperscissors/Game.java
+++ src/main/java/com/ashraymehta/rockpaperscissors/Game.java
@@ -1,6 +1,7 @@
 package com.ashraymehta.rockpaperscissors;
 
 import com.ashraymehta.rockpaperscissors.players.Player;
+import com.ashraymehta.rockpaperscissors.players.PlayerSelectionComparator;
 
 import java.util.Optional;
 
@@ -8,7 +9,7 @@ public class Game {
     private final Player firstPlayer;
     private final Player secondPlayer;
 
-    public Game(Player firstPlayer, Player secondPlayer) {
+    public Game(Player firstPlayer, Player secondPlayer, PlayerSelectionComparator playerSelectionComparator) {
         this.firstPlayer = firstPlayer;
         this.secondPlayer = secondPlayer;
     }
--- src/test/java/com/ashraymehta/rockpaperscissors/GameTest.java
+++ src/test/java/com/ashraymehta/rockpaperscissors/GameTest.java
@@ -2,11 +2,14 @@ package com.ashraymehta.rockpaperscissors;
 
 import com.ashraymehta.rockpaperscissors.players.Player;
 import com.ashraymehta.rockpaperscissors.players.PlayerSelection;
+import com.ashraymehta.rockpaperscissors.players.PlayerSelectionComparator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.spotify.hamcrest.optional.OptionalMatchers.emptyOptional;
+import static com.spotify.hamcrest.optional.OptionalMatchers.optionalWithValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
 
@@ -14,12 +17,14 @@ class GameTest {
     private Game game;
     private Player firstPlayer;
     private Player secondPlayer;
+    private PlayerSelectionComparator playerSelectionComparator;
 
     @BeforeEach
     void setUp() {
         firstPlayer = mock(Player.class);
         secondPlayer = mock(Player.class);
-        game = new Game(firstPlayer, secondPlayer);
+        playerSelectionComparator = mock(PlayerSelectionComparator.class);
+        game = new Game(firstPlayer, secondPlayer, playerSelectionComparator);
     }
 
     @Test
@@ -34,9 +39,21 @@ class GameTest {
     void shouldDetermineTheWinnerAsNobodyIfTheFirstPlayerAndSecondPlayerPlayTheSameThing() {
         when(firstPlayer.play()).thenReturn(PlayerSelection.ROCK);
         when(secondPlayer.play()).thenReturn(PlayerSelection.ROCK);
+        when(playerSelectionComparator.compare(PlayerSelection.ROCK, PlayerSelection.ROCK)).thenReturn(0);
 
         final var winner = game.start();
 
         assertThat(winner, is(emptyOptional()));
+    }
+
+    @Test
+    void shouldDetermineTheWinnerAsFirstPlayerIfTheFirstPlayerBeatsTheSecondPlayer() {
+        when(firstPlayer.play()).thenReturn(PlayerSelection.ROCK);
+        when(secondPlayer.play()).thenReturn(PlayerSelection.SCISSORS);
+        when(playerSelectionComparator.compare(PlayerSelection.ROCK, PlayerSelection.SCISSORS)).thenReturn(1);
+
+        final var winner = game.start();
+
+        assertThat(winner, is(optionalWithValue(equalTo(firstPlayer))));
     }
 }

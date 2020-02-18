9c4581a9f54254814d51a8f153a746af5db34275
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-02-18 11:39:54 +0530

    Writing results to console.

--- src/main/java/com/ashraymehta/rockpaperscissors/game/GameRunner.java
+++ src/main/java/com/ashraymehta/rockpaperscissors/game/GameRunner.java
@@ -1,17 +1,21 @@
 package com.ashraymehta.rockpaperscissors.game;
 
+import com.ashraymehta.rockpaperscissors.io.Console;
 import com.ashraymehta.rockpaperscissors.players.Player;
 
 public class GameRunner {
     private final Game game;
+    private final Console console;
 
-    public GameRunner(Game game) {
+    public GameRunner(Game game, Console console) {
         this.game = game;
+        this.console = console;
     }
 
     public void run(Player firstPlayer, Player secondPlayer) {
         for (int counter = 0; counter < 100; counter++) {
-            game.start(firstPlayer, secondPlayer);
+            final var gameSummary = game.start(firstPlayer, secondPlayer);
+            console.writeLine(gameSummary.toString());
         }
     }
 }

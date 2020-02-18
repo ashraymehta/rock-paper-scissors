7caaf950d8948049837863afb072ea6e8c7727ea
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-02-18 08:53:09 +0530

    Added comparison between any PlayerSelection and itself in PlayerSelectionComparator.

--- src/main/java/com/ashraymehta/rockpaperscissors/players/PlayerSelectionComparator.java
+++ src/main/java/com/ashraymehta/rockpaperscissors/players/PlayerSelectionComparator.java
@@ -4,6 +4,9 @@ import java.util.Comparator;
 import java.util.Map;
 
 public class PlayerSelectionComparator implements Comparator<PlayerSelection> {
+    private final static int EQUALS = 0;
+    private final static int LESS_THAN = -1;
+    private final static int GREATER_THAN = 1;
     private static final Map<PlayerSelection, PlayerSelection> WHO_BEATS_WHOM = Map.of(
             PlayerSelection.ROCK, PlayerSelection.SCISSORS,
             PlayerSelection.PAPER, PlayerSelection.ROCK,
@@ -12,6 +15,9 @@ public class PlayerSelectionComparator implements Comparator<PlayerSelection> {
 
     @Override
     public int compare(PlayerSelection selection, PlayerSelection anotherSelection) {
-        return WHO_BEATS_WHOM.get(selection) == anotherSelection ? 1 : -1;
+        if (selection == anotherSelection) {
+            return EQUALS;
+        }
+        return WHO_BEATS_WHOM.get(selection) == anotherSelection ? GREATER_THAN : LESS_THAN;
     }
 }

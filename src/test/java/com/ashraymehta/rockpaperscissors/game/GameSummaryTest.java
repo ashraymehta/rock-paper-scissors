0cab0a96112757ea78da6337631c358d8f7c1bcc
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-02-18 11:09:07 +0530

    GameSummary can describe itself in String format. [WIP]

--- src/test/java/com/ashraymehta/rockpaperscissors/game/GameSummaryTest.java
+++ src/test/java/com/ashraymehta/rockpaperscissors/game/GameSummaryTest.java
@@ -2,15 +2,18 @@ package com.ashraymehta.rockpaperscissors.game;
 
 import com.ashraymehta.rockpaperscissors.players.Player;
 import com.ashraymehta.rockpaperscissors.players.PlayerSelection;
+import com.ashraymehta.rockpaperscissors.players.RandomPlayer;
 import com.ashraymehta.rockpaperscissors.players.RockingPlayer;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
+import java.util.Random;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.mock;
 
 class GameSummaryTest {
 
@@ -34,5 +37,18 @@ class GameSummaryTest {
         assertNotEquals(aGameSummary, new GameSummary(Map.of(), winner));
         assertNotEquals(aGameSummary, new GameSummary(Map.of(), anotherPlayer));
         assertNotEquals(aGameSummary.hashCode(), new GameSummary(Map.of(), winner).hashCode());
+    }
+
+    @Test
+    void shouldHaveAStringRepresentationOfGameSummary() {
+        final var winner = new RockingPlayer("Player 1");
+        final var anotherPlayer = new RandomPlayer("Player 2", mock(Random.class));
+
+        final var summary = new GameSummary(Map.of(winner, PlayerSelection.ROCK,
+                anotherPlayer, PlayerSelection.SCISSORS), winner);
+
+        assertThat(summary.toString(), is("Player 1 selected: ROCK" + System.lineSeparator() +
+                "Player 2 selected: SCISSORS" + System.lineSeparator() +
+                "Winner: Player 1!"));
     }
 }

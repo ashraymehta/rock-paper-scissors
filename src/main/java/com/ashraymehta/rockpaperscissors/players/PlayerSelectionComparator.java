0fc7e03d1c4a2c0685f320745a35bb7ef15daf92
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-02-18 08:45:23 +0530

    Created PlayerSelectionComparator to compare ROCK and SCISSORS.

--- src/main/java/com/ashraymehta/rockpaperscissors/players/PlayerSelectionComparator.java
+++ src/main/java/com/ashraymehta/rockpaperscissors/players/PlayerSelectionComparator.java
@@ -1,10 +1,15 @@
 package com.ashraymehta.rockpaperscissors.players;
 
 import java.util.Comparator;
+import java.util.Map;
 
 public class PlayerSelectionComparator implements Comparator<PlayerSelection> {
+    private static final Map<PlayerSelection, PlayerSelection> WHO_BEATS_WHOM = Map.of(
+            PlayerSelection.ROCK, PlayerSelection.SCISSORS
+    );
+
     @Override
     public int compare(PlayerSelection selection, PlayerSelection anotherSelection) {
-        return 0;
+        return WHO_BEATS_WHOM.get(selection) == anotherSelection ? 1 : -1;
     }
 }

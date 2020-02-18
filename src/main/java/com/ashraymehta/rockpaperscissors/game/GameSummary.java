10f13e80a4e13b1be4ebc8c484bff664dad1351f
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-02-18 10:16:40 +0530

    Created class GameSummary to hold Game run related data.

--- src/main/java/com/ashraymehta/rockpaperscissors/game/GameSummary.java
+++ src/main/java/com/ashraymehta/rockpaperscissors/game/GameSummary.java
@@ -2,10 +2,25 @@ package com.ashraymehta.rockpaperscissors.game;
 
 import com.ashraymehta.rockpaperscissors.players.Player;
 
+import java.util.Objects;
+
 public class GameSummary {
     private final Player winner;
 
     public GameSummary(Player winner) {
         this.winner = winner;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameSummary that = (GameSummary) o;
+        return Objects.equals(winner, that.winner);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(winner);
     }
 }

fb0eb3e661f3c1cb00641f85f21dca464664272d
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-02-18 07:59:23 +0530

    Introduced class Game and interface Player. Asking players to play when the game starts.

--- src/main/java/com/ashraymehta/rockpaperscissors/Game.java
+++ src/main/java/com/ashraymehta/rockpaperscissors/Game.java
@@ -3,9 +3,16 @@ package com.ashraymehta.rockpaperscissors;
 import com.ashraymehta.rockpaperscissors.players.Player;
 
 public class Game {
+    private final Player firstPlayer;
+    private final Player secondPlayer;
+
     public Game(Player firstPlayer, Player secondPlayer) {
+        this.firstPlayer = firstPlayer;
+        this.secondPlayer = secondPlayer;
     }
 
     public void start() {
+        firstPlayer.play();
+        secondPlayer.play();
     }
 }

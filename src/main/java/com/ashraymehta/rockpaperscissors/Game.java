3a8d8c0f91953478a91e601c5b53693717347e97
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-02-18 09:06:24 +0530

    Game determines the winner as firstPlayer if they beat the secondPlayer.

--- src/main/java/com/ashraymehta/rockpaperscissors/Game.java
+++ src/main/java/com/ashraymehta/rockpaperscissors/Game.java
@@ -8,15 +8,19 @@ import java.util.Optional;
 public class Game {
     private final Player firstPlayer;
     private final Player secondPlayer;
+    private final PlayerSelectionComparator playerSelectionComparator;
 
     public Game(Player firstPlayer, Player secondPlayer, PlayerSelectionComparator playerSelectionComparator) {
         this.firstPlayer = firstPlayer;
         this.secondPlayer = secondPlayer;
+        this.playerSelectionComparator = playerSelectionComparator;
     }
 
     public Optional<Player> start() {
-        firstPlayer.play();
-        secondPlayer.play();
+        final var comparisonResult = playerSelectionComparator.compare(firstPlayer.play(), secondPlayer.play());
+        if (comparisonResult > 0) {
+            return Optional.of(firstPlayer);
+        }
         return Optional.empty();
     }
 }

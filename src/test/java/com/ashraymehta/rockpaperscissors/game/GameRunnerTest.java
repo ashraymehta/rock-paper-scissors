66f5c1e027a39cb3cf7f4617ea008a1c654dc8e8
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-02-18 11:39:10 +0530

    Writing results to console. [WIP]

--- src/test/java/com/ashraymehta/rockpaperscissors/game/GameRunnerTest.java
+++ src/test/java/com/ashraymehta/rockpaperscissors/game/GameRunnerTest.java
@@ -1,11 +1,15 @@
 package com.ashraymehta.rockpaperscissors.game;
 
+import com.ashraymehta.rockpaperscissors.io.Console;
 import com.ashraymehta.rockpaperscissors.players.Player;
+import com.ashraymehta.rockpaperscissors.players.PlayerSelection;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Map;
 
 import static org.mockito.Mockito.*;
 
@@ -18,13 +22,20 @@ class GameRunnerTest {
     @Mock
     private Game game;
 
+    @Mock
+    private Console console;
+
     @Test
-    void shouldRunHundredIterationsOfGame() {
+    void shouldPrintResultsForHundredIterationsOfGame() {
         final var firstPlayer = mock(Player.class);
         final var secondPlayer = mock(Player.class);
+        final var gameSummary = new GameSummary(Map.of(firstPlayer, PlayerSelection.ROCK,
+                secondPlayer, PlayerSelection.PAPER), secondPlayer);
+        when(game.start(firstPlayer, secondPlayer)).thenReturn(gameSummary);
 
         gameRunner.run(firstPlayer, secondPlayer);
 
         verify(game, times(100)).start(firstPlayer, secondPlayer);
+        verify(console, times(100)).writeLine(gameSummary.toString());
     }
 }

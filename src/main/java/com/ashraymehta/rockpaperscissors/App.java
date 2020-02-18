ef5e4e7d434551a2f7a7d1395b43bd88194ad814
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-02-18 11:48:13 +0530

    Wired up everything from App.

--- src/main/java/com/ashraymehta/rockpaperscissors/App.java
+++ src/main/java/com/ashraymehta/rockpaperscissors/App.java
@@ -1,11 +1,21 @@
 package com.ashraymehta.rockpaperscissors;
 
+import com.ashraymehta.rockpaperscissors.game.Game;
+import com.ashraymehta.rockpaperscissors.game.GameRunner;
+import com.ashraymehta.rockpaperscissors.io.Console;
+import com.ashraymehta.rockpaperscissors.players.PlayerSelectionComparator;
+import com.ashraymehta.rockpaperscissors.players.RandomPlayer;
+import com.ashraymehta.rockpaperscissors.players.RockingPlayer;
+
+import java.util.Random;
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        final var gameRunner = new GameRunner(new Game(new PlayerSelectionComparator()), new Console());
+        gameRunner.run(new RockingPlayer("Player 1"), new RandomPlayer("Player 2", new Random()));
     }
 }

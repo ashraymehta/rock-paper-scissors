58cf92d4b9d0d7a26cb2c05d848a9d15cf28f05c
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-02-18 09:21:00 +0530

    Added RandomPlayer who plays randomly.

--- src/main/java/com/ashraymehta/rockpaperscissors/players/RandomPlayer.java
+++ src/main/java/com/ashraymehta/rockpaperscissors/players/RandomPlayer.java
@@ -1,8 +1,17 @@
 package com.ashraymehta.rockpaperscissors.players;
 
+import java.util.Random;
+
 public class RandomPlayer implements Player {
+    private final Random random;
+
+    public RandomPlayer(Random random) {
+        this.random = random;
+    }
+
     @Override
     public PlayerSelection play() {
-        return null;
+        final var playerSelections = PlayerSelection.values();
+        return playerSelections[random.nextInt(playerSelections.length)];
     }
 }

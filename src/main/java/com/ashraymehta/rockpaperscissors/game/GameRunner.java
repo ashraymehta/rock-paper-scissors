5db58e8b1d8319e921985156f09563f7058a6d40
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-02-18 11:32:45 +0530

    Created GameRunner to run multiple iterations of the game.

--- src/main/java/com/ashraymehta/rockpaperscissors/game/GameRunner.java
+++ src/main/java/com/ashraymehta/rockpaperscissors/game/GameRunner.java
@@ -3,7 +3,15 @@ package com.ashraymehta.rockpaperscissors.game;
 import com.ashraymehta.rockpaperscissors.players.Player;
 
 public class GameRunner {
-    public void run(Player firstPlayer, Player secondPlayer) {
+    private final Game game;
 
+    public GameRunner(Game game) {
+        this.game = game;
+    }
+
+    public void run(Player firstPlayer, Player secondPlayer) {
+        for (int counter = 0; counter < 100; counter++) {
+            game.start(firstPlayer, secondPlayer);
+        }
     }
 }
